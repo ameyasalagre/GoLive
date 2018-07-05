cccea11ab76a82f94066b0bc9e1964030ba62c19
Ameya Salagre <you@example.com>
2018-07-05 23:43:49 +0530

    unknown design changes

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: false}));   // to support URL-encoded bodies
 
 //intializing route files
-app.set('views', path.join(__dirname, './views/public'));
+app.set('views', path.join(__dirname, './views/pages'));
+app.use(express.static(__dirname + '/views/css'));
+
 
 app.set('view engine', 'ejs');
 const home = require('./routes/home');
